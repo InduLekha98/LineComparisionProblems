60aa9a17c8b287e5a472a50fc615d73325a5f28c
InduLekha98 <rindulekha09@gmail.com>
2021-02-19 00:06:33 +0530

    Refactored:Refactored the code to use methods

--- LineComparision.java
+++ LineComparision.java
@@ -1,48 +1,39 @@
-import java.util.Scanner;
-public class LineComparision
-{
-	public static void main(String[] args)
-	{
-	  System.out.println("Calculate Length Between Two Point");
-	  int x1,x2,y1,y2;
-	  double Length1;
-	  Scanner sc=new Scanner(System.in);
-	  System.out.println("enter x1=");
-	  x1=sc.nextInt();
-	  System.out.println("enter x2=");
-	  x2=sc.nextInt();
-	  System.out.println("enter y1=");
-	  y1=sc.nextInt();
-	  System.out.println("enter y2=");
-	  y2=sc.nextInt();
-	  Length1=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-	  System.out.println("Length Between"+"("+x1+","+y1+"),"+"("+x2+","+y2+")="+Length1);
-	  int p1,p2,q1,q2;
-	  double Length2;
-	  System.out.println("enter p1 =");
-	  p1=sc.nextInt();
-	  System.out.println("enter p2 =");
-	  p2=sc.nextInt();
-	  System.out.println("enter q1 =");
-	  q1=sc.nextInt();
-	  System.out.println("enter q2 =");
-	  q2=sc.nextInt();
-	  Length2=Math.sqrt((p2-p1)*(p2-p1) + (q2-q1)*(q2-q1));
-	  System.out.println("Length between"+"("+p1+","+q1+"),"+"("+p2+","+q2+")="+Length2);
-	  Double obj1=new Double(Length1);
-          Double obj2=new Double(Length2);
-	  int comparevalue=obj1.compareTo(obj2);
-	  if(comparevalue==0)
-	  {
-	    System.out.println("Both lines are same");
-	  }
-	  else if(comparevalue>0)
-	  {
-	    System.out.println("Length1 is greater than Length2");
-	  }
-	  else
-	  {
-	    System.out.println("Length1 is less than Length2");
-	  }
+import java.lang.Math;
+
+class Compare {
+	public void compareTo(double len_line1, double len_line2) {
+          if (len_line1 > len_line2)
+                System.out.println("Length of line 1 is greater than Length of line 2");
+          else if (len_line1 < len_line2)
+                System.out.println("Length of line 1 is lesser than Length of line 2");
+          else
+                System.out.println("Both lines are equal");
+                System.out.println("Length of line1 is "+len_line1);
+                System.out.println("Length of line2 is "+len_line2);
+	}
+}
+
+public class LineComparision {
+	private final double x1;
+	private final double y1;
+	private final double x2;
+	private final double y2;
+
+	public LineComparision(double x1, double y1, double x2, double y2) {
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparision Computation Program");
+		LineComparision line1 = new LineComparision(5.5, 2.9, 3.6, 5.8);
+		LineComparision line2 = new LineComparision(9.4, 4.7, 5.2, 8.2);
+        	double length_line1 = Math.sqrt(Math.pow((line1.x2-line1.x1),2) + Math.pow((line1.y2-line1.y1),2));
+		double length_line2 = Math.sqrt(Math.pow((line2.x2-line2.x1),2) + Math.pow((line2.y2-line2.y1),2));
+		Compare comparision = new Compare();
+		comparision.compareTo(length_line1, length_line2);
 	}
 }
